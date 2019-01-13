2ba35e2f03c0571be812c0ea86a3d82fe6a4485b
Hamza-Ashour <hamzaashoor949@hotmail.com>
2019-01-13 16:10:15 +0200

    store shape created

--- src/store/index.js
+++ src/store/index.js
@@ -1,154 +1,114 @@
 //@flow
 import { createStore } from "redux";
 import rootReducer from "../reducers/root";
-// import ID from "../classes/ID";
+import ID from "../classes/ID";
+import RadioGroup from "../classes/RadioGroup";
+
+type User = {
+  id: ID,
+  username: string,
+  email: string,
+  isAdmin: boolean,
+  isAuthor: boolean
+};
+
+type Author = {
+  id: ID,
+  username: string
+};
+
+type Category = {
+  id: ID,
+  name: string
+};
+
+type Book = {
+  id: ID,
+  name: string,
+  author: ?Author,
+  categoryList: Array<Category>
+};
+
+type InputField = {
+  value: string,
+  error: ?string
+};
+
+type CheckBoxField<Type> = {
+  value: Type,
+  selected: boolean
+};
 
 export type State = {|
-  user: ?{
-    id: string,
-    username: string,
-    email: string
+  user: ?User,
+  loginForm: ?{
+    email: InputField,
+    password: InputField
   },
-  // loginForm: PropTypes.oneOfType([
-  //   PropTypes.instanceOf(Null),
-  //   PropTypes.shape({
-  //     email: PropTypes.shape({
-  //       value: PropTypes.string.isRequired,
-  //       error: PropTypes.string.isRequired
-  //     }).isRequired,
-  //     password: PropTypes.shape({
-  //       value: PropTypes.string.isRequired,
-  //       error: PropTypes.string.isRequired
-  //     }).isRequired
-  //   })
-  // ]).isRequired,
-  // bookList: PropTypes.arrayOf(
-  //   PropTypes.shape({
-  //     id: PropTypes.instanceOf(ID).isRequired,
-  //     name: PropTypes.string.isRequired,
-  //     author: PropTypes.oneOfType([
-  //       PropTypes.instanceOf(Null),
-  //       PropTypes.shape({
-  //         id: PropTypes.instanceOf(ID).isRequired,
-  //         name: PropTypes.string.isRequired
-  //       })
-  //     ]).isRequired,
-  //     categories: PropTypes.arrayOf(
-  //       PropTypes.shape({
-  //         id: PropTypes.instanceOf(ID).isRequired,
-  //         name: PropTypes.string.isRequired
-  //       })
-  //     ).isRequired
-  //   })
-  // ).isRequired,
-  // bookSearchForm: PropTypes.oneOfType([
-  //   PropTypes.instanceOf(Null),
-  //   PropTypes.shape({
-  //     bookName: PropTypes.string.isRequired,
-  //     authorName: PropTypes.string.isRequired,
-  //     categories: PropTypes.arrayOf(
-  //       PropTypes.shape({
-  //         id: PropTypes.instanceOf(ID).isRequired,
-  //         selected: PropTypes.bool.isRequired
-  //       })
-  //     ).isRequired
-  //   })
-  // ]).isRequired,
-  // bookForm: PropTypes.oneOfType([
-  //   PropTypes.instanceOf(Null),
-  //   PropTypes.shape({
-  //     type: PropTypes.oneOf(["create", "update"]).isRequired,
-  //     id: PropTypes.instanceOf(ID),
-  //     name: PropTypes.shape({
-  //       value: PropTypes.string.isRequired,
-  //       error: PropTypes.string.isRequired
-  //     }).isRequired,
-  //     authors: PropTypes.oneOfType([
-  //       PropTypes.instanceOf(Null),
-  //       PropTypes.arrayOf(
-  //         PropTypes.shape({
-  //           id: PropTypes.instanceOf(ID).isRequired,
-  //           name: PropTypes.string.isRequired,
-  //           selected: PropTypes.bool.isRequired
-  //         })
-  //       )
-  //     ]).isRequired,
-  //     categories: PropTypes.arrayOf(
-  //       PropTypes.shape({
-  //         id: PropTypes.instanceOf(ID).isRequired,
-  //         name: PropTypes.string.isRequired,
-  //         selected: PropTypes.bool.isRequired
-  //       })
-  //     ).isRequired
-  //   })
-  // ]).isRequired,
-  // categoryList: PropTypes.arrayOf(
-  //   PropTypes.shape({
-  //     id: PropTypes.instanceOf(ID).isRequired,
-  //     name: PropTypes.string.isRequired
-  //   })
-  // ).isRequired,
-  // categoryForm: PropTypes.oneOfType([
-  //   PropTypes.instanceOf(Null),
-  //   PropTypes.shape({
-  //     type: PropTypes.oneOf(["create", "update"]).isRequired,
-  //     id: PropTypes.instanceOf(ID),
-  //     name: PropTypes.shape({
-  //       value: PropTypes.string.isRequired,
-  //       error: PropTypes.string.isRequired
-  //     }).isRequired
-  //   })
-  // ]).isRequired,
-  // userList: PropTypes.arrayOf(
-  //   PropTypes.shape({
-  //     id: PropTypes.instanceOf(ID).isRequired,
-  //     username: PropTypes.string.isRequired,
-  //     email: PropTypes.string.isRequired,
-  //     password: PropTypes.string.isRequired,
-  //     isAdmin: PropTypes.bool.isRequired,
-  //     isAuthor: PropTypes.bool.isRequired
-  //   })
-  // ).isRequired,
-  // userForm: PropTypes.oneOfType([
-  //   PropTypes.instanceOf(Null),
-  //   PropTypes.shape({
-  //     type: PropTypes.oneOf(["create", "update", "signup"]).isRequired,
-  //     id: PropTypes.instanceOf(ID),
-  //     username: PropTypes.shape({
-  //       value: PropTypes.string.isRequired,
-  //       error: PropTypes.string.isRequired
-  //     }).isRequired,
-  //     email: PropTypes.shape({
-  //       value: PropTypes.string.isRequired,
-  //       error: PropTypes.string.isRequired
-  //     }).isRequired,
-  //     password: PropTypes.shape({
-  //       value: PropTypes.string.isRequired,
-  //       error: PropTypes.string.isRequired
-  //     }).isRequired,
-  //     isAdmin: PropTypes.oneOfType([PropTypes.instanceOf(Null), PropTypes.bool])
-  //       .isRequired,
-  //     isAuthor: PropTypes.oneOfType([
-  //       PropTypes.instanceOf(Null),
-  //       PropTypes.bool
-  //     ]).isRequired
-  //   })
-  // ]).isRequired,
+  bookList: Array<Book>,
+  bookSearchForm: ?{
+    bookName: InputField,
+    authorName: InputField,
+    categoryList: Array<CheckBoxField<ID>>
+  },
+  createBookForm: ?{
+    name: InputField,
+    author: ?RadioGroup<Author>,
+    categoryList: Array<CheckBoxField<Category>>
+  },
+  updateBookForm: ?{
+    +id: ID,
+    name: InputField,
+    author: ?RadioGroup<Author>,
+    categoryList: Array<CheckBoxField<Category>>
+  },
+  categoryList: Array<Category>,
+  createCategoryForm: ?{
+    name: InputField
+  },
+  updateCategoryForm: ?{
+    +id: ID,
+    name: InputField
+  },
+  userList: Array<User>,
+  signupUserForm: ?{
+    email: InputField,
+    username: InputField,
+    password: InputField
+  },
+  createUserForm: ?{
+    username: InputField,
+    email: InputField,
+    password: InputField,
+    isAdmin: boolean,
+    isAuthor: boolean
+  },
+  updateUserForm: ?{
+    +id: ID,
+    username: InputField,
+    email: InputField,
+    password: InputField,
+    isAdmin: boolean,
+    isAuthor: boolean
+  },
   mobileMenu: boolean,
   authorized: boolean
-  // authorized: PropTypes.bool.isRequired
 |};
 
 const INITIAL_STATE: State = {
   user: null,
-  // loginForm: null,
-  // bookList: [],
-  // bookSearchForm: null,
-  // bookForm: null,
-  // categoryList: [],
-  // categoryForm: null,
-  // userList: [],
-  // userForm: null,
+  loginForm: null,
+  bookList: [],
+  bookSearchForm: null,
+  createBookForm: null,
+  updateBookForm: null,
+  categoryList: [],
+  createCategoryForm: null,
+  updateCategoryForm: null,
+  userList: [],
+  signupUserForm: null,
+  createUserForm: null,
+  updateUserForm: null,
   mobileMenu: false,
   authorized: true
 };
